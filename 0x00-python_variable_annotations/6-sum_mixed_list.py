7c94ef78809df03c9f9a3a6015fcba49f596d849
DTAJ095 <albanjaures26@gmail.com>
2024-06-06 20:29:14 +0100

    added

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """ Type-annotated function sum_mixed_list which takes
 a list mxd_lst of integers and floats.
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ the tuple is the string k.
 The second element is the square of the int/float v
 and should be annotated as a float.
 """
-
+from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Return tuple of string and float """
